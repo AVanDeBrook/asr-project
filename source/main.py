6f5370a4099e329462d18e41dc56872700097cf6
avandebrook <avandebrook5@gmail.com>
2023-02-17 10:22:59 -0500

    Update main to reflect design changes.

--- source/main.py
+++ source/main.py
@@ -1,49 +1,53 @@
-import json
-import matplotlib.pyplot as plt
 import logging
 import os
-from logging import INFO
-from numpy.random import default_rng
+from logging import DEBUG, INFO
 from typing import *
-from data import Data, ATCCompleteData, ATCOSimData, ATCO2SimData, ZCUATCDataset
+
+import matplotlib.pyplot as plt
+from data import ATCCompleteData, ATCO2SimData, ATCOSimData, Data, ZCUATCDataset
 from models import (
     Model,
     PretrainedFineTunedJasper,
-    PretrainedJasper,
     PretrainedFineTunedQuartzNet,
+    PretrainedJasper,
     PretrainedQuartzNet,
     RandomInitCTC,
 )
-from matplotlib.figure import Figure
+from numpy.random import default_rng
 
 logger = logging.getLogger(__name__)
+
+# root dataset paths corresponding to data analysis classes
+datasets: Dict[str, Data] = {
+    # TODO: find a way to sync file paths across computers (shell/env var, config file?)
+    "/home/students/vandebra/programming/thesis_data/atc0_comp": ATCCompleteData,
+    "/home/students/vandebra/programming/thesis_data/atcosim/": ATCOSimData,
+    "/home/students/vandebra/programming/thesis_data/ATCO2-ASRdataset-v1_beta": ATCO2SimData,
+    # "/home/students/vandebra/programming/thesis_data/ZCU_CZ_ATC": ZCUATCDataset,
+}
+
+# name, model, number of epochs to train
+models: List[Tuple[str, Model, int]] = [
+    # Jasper models
+    ("jasper_pretrained.nemo", PretrainedJasper, None),
+    ("jasper_finetuned.nemo", PretrainedFineTunedJasper, 100),
+    # # QuartzNet models
+    ("quartznet_pretrained.nemo", PretrainedQuartzNet, None),
+    ("quartznet_finetuned.nemo", PretrainedFineTunedQuartzNet, 100),
+    # "Out-of-the-Box" models
+    ("ctc_randominit.nemo", RandomInitCTC, 300)
+]
 
 
 if __name__ == "__main__":
     # TODO: clean this main statement
     # random seed initialization (passed to data classes) for reproducible randomness
     # in results
-    logger.setLevel(INFO)
+    logger.setLevel(DEBUG)
     RANDOM_SEED: int = 1
     random = default_rng(RANDOM_SEED)
 
     plt.style.use("ggplot")
-
-    # root dataset paths corresponding to data analysis classes
-    datasets: Dict[str, Data] = {
-        # TODO: find a way to sync file paths across computers (shell/env var, config file?)
-        "/home/students/vandebra/programming/thesis_data/atc0_comp": ATCCompleteData,
-        "/home/students/vandebra/programming/thesis_data/atcosim/": ATCOSimData,
-        "/home/students/vandebra/programming/thesis_data/ATCO2-ASRdataset-v1_beta": ATCO2SimData,
-        # "/home/students/vandebra/programming/thesis_data/ZCU_CZ_ATC": ZCUATCDataset,
-    }
-
-    # datasets combined into one large dataset (also transformed into a common format)
-    concat_dataset = []
-
-    # find number of unique tokens across datasets
-    num_unique_tokens = 0
-    num_tokens = 0
 
     for root_path, data_class in datasets.items():
         data_analysis: Data = data_class(data_root=root_path, random_seed=RANDOM_SEED)
@@ -54,76 +58,35 @@ if __name__ == "__main__":
 
         # tokens in dataset
         token_freq = data_analysis.token_freq_analysis(normalize=True)
-        num_unique_tokens += len(token_freq.keys())
-        for counts in token_freq.values():
-            num_tokens += counts[0]
-
-        # concatenate individual datasets into one larger dataset
-        concat_dataset.extend(
-            data_analysis.dump_manifest(
-                f"manifests/{data_analysis.name.replace(' ', '_')}_manifest.json",
-                return_list=True,
-            )
-        )
-
-    # TODO: create a function for and clean up this mess
-    # dump to a manifest
-    with open("manifests/all_manifest.json", "w", encoding="utf-8") as f:
-        for entry in concat_dataset:
-            f.write(json.dumps(entry))
-            f.write("\n")
-
-    random.shuffle(concat_dataset)
-    num_train_samples = int(len(concat_dataset) * 0.8)
-    num_valid_samples = int(num_train_samples * 0.2)
-
-    with open("manifests/train_manifest.json", "w", encoding="utf-8") as f:
-        for entry in concat_dataset[: (num_train_samples - num_valid_samples)]:
-            f.write(json.dumps(entry))
-            f.write("\n")
-
-    with open("manifests/validation_manifest.json", "w", encoding="utf-8") as f:
-        for entry in concat_dataset[
-            (num_train_samples - num_valid_samples) : num_train_samples
-        ]:
-            f.write(json.dumps(entry))
-            f.write("\n")
-
-    with open("manifests/test_manifest.json", "w", encoding="utf-8") as f:
-        for entry in concat_dataset[num_train_samples:]:
-            f.write(json.dumps(entry))
-            f.write("\n")
-
-    print(f"Total samples: {len(concat_dataset)}")
-    print(f"Total unique tokens (across all datasets): {num_unique_tokens}")
-    print(f"Total tokens: {num_tokens}")
 
     """ Model Training/Testing """
 
-    # name, model pairs
-    models: Dict[str, Model] = {
-        # Jasper models
-        "jasper_pretrained.nemo": PretrainedJasper,
-        "jasper_finetuned.nemo": PretrainedFineTunedJasper,
-        # # QuartzNet models
-        "quartznet_pretrained.nemo": PretrainedQuartzNet,
-        "quartznet_finetuned.nemo": PretrainedFineTunedQuartzNet,
-        # "Out-of-the-Box" models
-        "ctc_randominit.nemo": RandomInitCTC,
-    }
-
     # TODO
-    model_wers = {}
-    for name, model in models.items():
+    model_wers: List[Tuple[str, float]] = []
+    for name, model_class, epochs in models:
         # create model
-        model: Model = model(checkpoint_name=name)
+        model: Model = model_class(checkpoint_name=name)
+
         # train
-        # if not os.path.exists(f"checkpoints/{name}"):
-        #     model.fit()
+        if not os.path.exists(f"checkpoints/{name}") and epochs is not None:
+            model.training_setup(
+                training_manifest_path="manifests/train_manifest.json",
+                validation_manifest_path="manifests/valid_manifest.json",
+                accelerator="gpu",
+                max_epochs=epochs
+            )
+            model.fit()
+
         # test
-        model_wers[name] = model.test(log_prediction=False)
+        model.testing_setup(test_manifest_path="manifests/test_manifest.json")
+        model_wers.append(
+            tuple([
+                name,
+                model.test()
+            ])
+        )
 
     print("WERs:")
     print("-----------")
-    for name, wer in model_wers.items():
+    for name, wer in model_wers:
         print(f"{name}: {wer}")
