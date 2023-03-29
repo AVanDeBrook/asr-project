3d5c99845458a4e55b68f1d8848d03c9f77644ba
Aaron Van De Brook <avandebrook5@gmail.com>
2023-03-29 14:03:18 -0400

    False start data point collection.

--- source/main.py
+++ source/main.py
@@ -76,8 +76,8 @@ if __name__ == "__main__":
     with open("manifests/dataset_stats.json", "w", encoding="utf-8") as f:
         f.write(json.dumps(dataset_info, indent=1))
 
+    # concatenate everything to first object
     for o in data_objects[1:]:
-        # concatenate everything to first object
         data_objects[0].concat(o)
 
     print(f"Unique tokens: {data_objects[0].unique_tokens}")
